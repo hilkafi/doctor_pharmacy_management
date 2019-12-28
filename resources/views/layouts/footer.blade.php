ec020ee8d1f064cd004810f3e60fa68326afbc63
hilkafi <kafi.bru@gmail.com>
2019-12-28 12:35:28 +0600

    copyright section modified.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,8 @@
 </div>
 <footer>
-  <div class="foo">
-    <div class="card">
-      <div class="card-body">
-        <h6 class="card-header text-center bg-primary" style="color: white;">
-          &copy;2019 - All Rights Reserve By Pharmamarket
-        </h6>
-      </div>
-    </div>
+  <div class="footer">
+          &copy;2019 - All Rights Reserve By Pharma Market Info
+          <span class="pull-right"><i>Developed By</i>- <a href="#" style="color: white">BITS</a></span>
   </div>
 </footer>
 </body>
